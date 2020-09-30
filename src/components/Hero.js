a50c2d369605d9f3c02f29b5070b280470f8bc2a
Aarsh Oza <aarshoza@gmail.com>
2020-09-30 10:53:30 -0400

    Links Updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
 
 function Hero(props){
-
+  console.log(props)
   return (
     <Jumbotron className="bg-transparent jumbotron-fluid p-0">
       <Container fluid={true}>
         <Row className="justify-content-center py-5">
           <Col md={8} sm={12}>
-            {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1>}
-            {props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3>}
+            {props.title && <h1 style={{fontSize:'10vw'}} className="">{props.title}</h1>}
+            {props.subTitle && <h3 style={{ fontSize: '6vw' }} className="">{props.subTitle}</h3>}
             {props.text && <h3 className="lead font-weight-light">{props.text}</h3>}
           </Col>
         </Row>
